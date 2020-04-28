e809eba6c27945027147b6f40c1ed75bcfcce9e7
elhiyane <64174770+elhiyane@users.noreply.github.com>
2020-04-28 01:32:40 -0400

    Update logCall.java

--- src/logCall.java
+++ src/logCall.java
@@ -1,5 +1,9 @@
 public class logCall {
     public static void main(String[] args) {
         System.out.println("Calling Muhtar....");
+        
+        
+       //i will see this in intellij
+        //am I be amazed?
     }
 }
